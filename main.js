132139a779b38011e5fecd0bdb6fac9f60d3a02b
Adnan <156185192+godrivelog@users.noreply.github.com>
2024-03-08 03:32:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('login-form');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Simulate login validation (replace with your actual logic)
+    if (username === 'Sadik4u3' && password === 'sadik3241') {
+        
+        window.location.href = "https://godrivelive.github.io/Sadikgodrivelive";
+    } 
+    if (username === 'Moraladnan' && password === 'moralweb123') {
+        
+        window.location.href = "https://godrivelive.github.io/Moralgodrivelive";
+    }
+    if (username === 'Video' && password === 'player') {
+        
+        window.location.href = "https://godrivelive.github.io/own";
+    } else {
+        window.location.href = "https://godrivelog.github.io/login/bokachoda.html";
+   
+    }
+});
+
